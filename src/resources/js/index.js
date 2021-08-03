9ad9bd3f812e47ca7d7fbb50c27ac4dd16179b89
Albin Ljungdahl <Albin.Ljungdal@se.experis.com>
2021-08-03 09:05:04 +0200

    feat: Implemented the work area and the laptop selection area

--- src/resources/js/index.js
+++ src/resources/js/index.js
@@ -1,12 +1,41 @@
 const balanceElement = document.getElementById("balance");
 const loanButtonElement = document.getElementById("loanButton");
-const outstandingLoanElement = document.getElementById("outstandingLoan")
-const outstandingLoanAmountElement = document.getElementById("outstandingLoanAmount")
+const outstandingLoanElement = document.getElementById("outstandingLoan");
+const outstandingLoanAmountElement = document.getElementById("outstandingLoanAmount");
 
-let balance = 100;
+const workButtonElement = document.getElementById("workButton");
+const bankButtonElement = document.getElementById("bankButton");
+const repayLoanButtonElement = document.getElementById("repayLoanButton");
+const payElement = document.getElementById("pay");
+
+const laptopSelectionElement = document.getElementById("laptopSelection");
+const featuresListElement = document.getElementById("featuresList");
+
+let balance = 0;
 let outstandingLoan = 0;
 
-const takeLoan = () => {
+let pay = 0;
+
+let laptops = [];
+
+fetch("https://noroff-komputer-store-api.herokuapp.com/computers")
+    .then(response => response.json())
+    .then(data => laptops = data)
+    .then(laptops => addLaptopsToList(laptops));
+
+const addLaptopsToList = (laptops) => {
+    laptops.forEach(x => addLaptopToList(x));
+    addLaptopSpecs(laptops[0]);
+}
+
+const addLaptopToList = (laptop) => {
+    const laptopElement = document.createElement("option");
+    laptopElement.value = laptop.id;
+    laptopElement.appendChild(document.createTextNode(laptop.title));
+    laptopSelectionElement.appendChild(laptopElement);
+}
+
+const handleLoanButtonClick = () => {
     if(outstandingLoan > 0) {
         alert("You may only have one loan at a time!");
         return;
@@ -29,13 +58,84 @@ const takeLoan = () => {
 const updateLoanBalance = () => {
     if(outstandingLoan > 0) {
         outstandingLoanElement.hidden = false;
+        repayLoanButtonElement.hidden = false;
+        outstandingLoanAmountElement.innerText = outstandingLoan + ' Kr.';
     }
 
-    outstandingLoanAmountElement.innerText = outstandingLoan + ' Kr.';
+    else {
+        outstandingLoanElement.hidden = true;
+        repayLoanButtonElement.hidden = true;
+        outstandingLoanAmountElement.innerText = '';
+    }
 }
 
 const updateBalance = () => {
     balanceElement.innerText = balance + ' Kr.';
 }
 
-loanButtonElement.addEventListener("click", takeLoan);
+
+
+
+
+const handleWorkButtonClick = () => {
+    pay += 100;
+    updatePay();
+}
+
+const handleBankButtonClick = () => {
+    if(outstandingLoan > 0) {
+        outstandingLoan -= (pay / 10);
+        pay -= (pay / 10);
+
+        if(outstandingLoan < 0) {
+            pay += (- outstandingLoan);
+            outstandingLoan = 0;
+        }
+
+        updateLoanBalance();
+    }
+
+    balance += pay;
+    pay = 0;
+    updateBalance();
+    updatePay();
+}
+
+const updatePay = () => {
+    payElement.innerText = pay + ' Kr.';
+}
+
+const handleRepayLoanButtonClick = () => {
+    if(pay > outstandingLoan) {
+        pay -= outstandingLoan;
+        outstandingLoan = 0;
+    }
+
+    else {
+        outstandingLoan -= pay;
+        pay = 0;
+    }
+
+    updatePay();
+    updateLoanBalance();
+}
+
+const handleLaptopSelectionChange = (e) => {
+    const selectedLaptop = laptops[e.target.selectedIndex];
+    addLaptopSpecs(selectedLaptop);
+}
+
+const addLaptopSpecs = (selectedLaptop) => {
+    featuresListElement.innerHTML = "";
+    selectedLaptop.specs.forEach(x => {
+        const feature = document.createElement("li");
+        feature.innerText = x;
+        featuresListElement.appendChild(feature);
+    })
+}
+
+loanButtonElement.addEventListener("click", handleLoanButtonClick);
+workButtonElement.addEventListener("click", handleWorkButtonClick);
+bankButtonElement.addEventListener("click", handleBankButtonClick);
+repayLoanButtonElement.addEventListener("click", handleRepayLoanButtonClick);
+laptopSelectionElement.addEventListener("change", handleLaptopSelectionChange);
